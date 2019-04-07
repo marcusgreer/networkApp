ff8a94aeba16947dba7b3be615f4497de499a7de
Michelle Ma <michell3@andrew.cmu.edu>
2019-04-06 23:21:48 -0400

    big tings

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var http= require('http').Server(app);
+var io - require('ip');
+app.use(express.static('/'));
+
+require("./controller/controller.js")(app, io);
+
+http.listen(8080, function() {
+	console.log("Node Server is set up and is listening on http://" + ip.address() + ":8080")
+})
